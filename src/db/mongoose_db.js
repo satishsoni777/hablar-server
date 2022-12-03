9bd7f074ff7e91a1ae9f41c6ba2b29a256f60c1a
satish <satish.kumar@jungleegames.com>
2022-12-03 19:57:26 +0530

    .

--- src/db/mongoose_db.js
+++ src/db/mongoose_db.js
@@ -1,30 +1,30 @@
 // import mongoose from "mongoose";
 // import {Config} from '../config/default.js'
 
-// export default async function connectMd() {
-//   console.log("OT adad");
+export default async function connectMd() {
+  console.log("OT adad");
 //   await mongoose.connect(Config.dbUrl).then(() => {
 //     console.log("Connected")
 //   }).catch((e) => { console.log("Not Connected", e) });
 
-// }
-// export async function inserOneData(data) {
+}
+export async function inserOneData(data) {
 
-//   try {
-//     const result = await data.save();
-//     if(result){
-//       console.log(result);
-//     }
-//   }
-//   catch (e) {
-//     console.log(e);
-//     throw e;
-//   }
-//   // mongoose.collection("users_profile").insertOne(data, function (err, res) {
-//   //   if (err) throw err;
-//   //   mongoose.close();
-//   // });
-// }
+  try {
+    const result = await data.save();
+    if(result){
+      console.log(result);
+    }
+  }
+  catch (e) {
+    console.log(e);
+    throw e;
+  }
+  // mongoose.collection("users_profile").insertOne(data, function (err, res) {
+  //   if (err) throw err;
+  //   mongoose.close();
+  // });
+}
 
 
 
