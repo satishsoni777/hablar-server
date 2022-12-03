b5e1534f979b033f64b15b621e3ccbf707264e98
satish <satish.kumar@jungleegames.com>
2022-12-03 18:00:28 +0530

    a

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,22 +3,26 @@ import express from "express";
 const router=express.Router();
 
 
-router.get('/download/link',function(req,res){
-    response.pipe(file);
-   file.on("finish", () => {
-       file.close();
-       console.log("Download Completed");
-   });
-})
+// router.get('/download/link',function(req,res){
+//     response.pipe(file);
+//    file.on("finish", () => {
+//        file.close();
+//        console.log("Download Completed");
+//    });
+// })
 
-router.get('/download', function(req, res){
-    const file = `${__dirname}/apk/app-cash-release.apk`;
-    res.download(file);
-  });
+// router.get('/download', function(req, res){
+//     const file = `${__dirname}/apk/app-cash-release.apk`;
+//     res.download(file);
+//   });
 
-  router.get('/download/poker.apk',function(req,res){
-    const file = `${__dirname}/apk/app-poker-release.apk`;
-    res.download(file);
-})
-
+//   router.get('/download/poker.apk',function(req,res){
+//     const file = `${__dirname}/apk/app-poker-release.apk`;
+//     res.download(file);
+// })
+router.get("hello",function (req,res){
+    res.send({
+        message:"Hello Duniya"
+    })
+});
 export default router;
